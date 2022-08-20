983ab692823b2b4d5467bb0812c1dfacc9c24eec
Yogi Anggara <yogianggaraaa@gmail.com>
2022-08-21 03:40:51 +0700

    create chat room component

---
+++ app/Models/Chat.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Chat extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    /**
+     * Get the user that owns the Chat
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
